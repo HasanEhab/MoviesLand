7a1f6536d53c8bdc5fdfae8b166c116b14728f92
Hassan Ehab <hassan_ehab96@yahoo.com>
2022-05-28 16:17:46 +0200

    navigation

--- src/components/MySlider.js
+++ src/components/MySlider.js
@@ -17,21 +17,21 @@ class MySlider extends Component {
         <Slider>
           <Slide index={0} className="slider__page">
           {this.props.allDetails.slice(5,12).map(movie=>{return(
-                                    <div key={movie.id} className="col"><NavLink  className="remove-style" to={movie.title?"/movie/:" + movie.id:"/serie/:" + movie.id}>
+                                    <div key={movie.id} className="col"><NavLink  className="remove-style" to={movie.title?"/MoviesLand/movie/:" + movie.id:"/serie/:" + movie.id}>
                                     <img src={this.state.imgFirstPart+movie.poster_path} className="d-block  mx-2 img-box" alt=""/>
                                     </NavLink></div>
                                 )})}
             </Slide>
           <Slide index={1} className="slider__page">
           {this.props.allDetails.slice(15,22).map(movie=>{return(
-                                    <div key={movie.id} className="col"><NavLink  className="remove-style" to={movie.title?"/movie/:" + movie.id:"/serie/:" + movie.id}>
+                                    <div key={movie.id} className="col"><NavLink  className="remove-style" to={movie.title?"/MoviesLand/movie/:" + movie.id:"/serie/:" + movie.id}>
                                     <img src={this.state.imgFirstPart+movie.poster_path} className="d-block  mx-2 img-box" alt=""/>
                                     </NavLink></div>
                                 )})}
             </Slide>
           <Slide index={2} className="slider__page">
           {this.props.allDetails.slice(30,37).map(movie=>{return(
-                                    <div key={movie.id} className="col"><NavLink  className="remove-style" to={movie.title?"/movie/:" + movie.id:"/serie/:" + movie.id}>
+                                    <div key={movie.id} className="col"><NavLink  className="remove-style" to={movie.title?"/MoviesLand/movie/:" + movie.id:"/serie/:" + movie.id}>
                                     <img src={this.state.imgFirstPart+movie.poster_path} className="d-block  mx-2 img-box" alt=""/>
                                     </NavLink></div>
                                 )})}
