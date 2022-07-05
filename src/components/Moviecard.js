1b885814304df047be174e08162fd57c20e0a519
Hassan Ehab <hassan_ehab96@yahoo.com>
2022-07-05 17:21:29 +0200

    rate fixed

--- src/components/Moviecard.js
+++ src/components/Moviecard.js
@@ -7,7 +7,7 @@ export default class Moviecard extends Component {
     return (
       <div className='moviecard'>
           <div className='moviecard__img'><img src={this.props.img}/></div>
-          <div className='moviecard__rate'>{this.props.rate}</div>
+          <div className='moviecard__rate'>{this.props.rate.toFixed(1)}</div>
           <div className='moviecard__name'>{this.props.name}</div>
       </div>
     )
